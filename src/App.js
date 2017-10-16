639fa47ffd9d62b6e0bf3bb9abb373f53e27c442
Mika Paulasaari <mika.paulasaari@idean.com>
2017-10-16 21:15:59 +0300

    add components and helpers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import firebase from './firebase'
+import Dashboard from './modules/Dashboard'
 
 class App extends Component {
+  state = {
+    items: []
+  }
+  componentWillMount() {
+    const cachedNumbers = JSON.parse(localStorage.getItem('lottoNumbers'))
+    if (cachedNumbers) {
+      const dateString = cachedNumbers.timestamp
+      const now = new Date().getTime().toString()
+      const week = 7 * 24 * 60 * 60 * 1000
+      if (dateString + week < now) {
+        console.log('too old numbers, fetch and cache')
+        this.getItems()
+      } else {
+        console.log('using cached numbers')
+        this.setState({
+          items: cachedNumbers.numbers,
+        })
+      }
+    } else {
+      console.log('first time loading, no cached numbers')
+      this.getItems()
+    }
+  }
+  getItems = () => {
+    const fbRef = firebase.database().ref()
+    fbRef.on('value', snapshot => {
+      this.setState({
+        items: snapshot.val(),
+      })
+      const cachedNumbers = {timestamp: new Date().getTime(), numbers: snapshot.val()}
+      localStorage.setItem('lottoNumbers', JSON.stringify(cachedNumbers))
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Dashboard items={this.state.items} />
+    )
   }
 }
 
-export default App;
+export default App
