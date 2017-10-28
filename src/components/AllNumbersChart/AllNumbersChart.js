fc2e09707095a9e74e958a26793017ba5630f4d7
Mika Paulasaari <mika.paulasaari@idean.com>
2017-10-28 22:58:26 +0300

    style updates

--- src/components/AllNumbersChart/AllNumbersChart.js
+++ src/components/AllNumbersChart/AllNumbersChart.js
@@ -20,11 +20,33 @@ class AllNumbersChart extends Component {
       <section className='AllNumbersChart section'>
         <h2>{title}</h2>
         <ResponsiveContainer width='100%' height={300}>
-          <BarChart data={numbers}>
-            <XAxis dataKey='number' interval='preserveStartEnd' />
-            <YAxis dataKey='count' />
-            <Tooltip />
-            <Bar dataKey='count' shape={<RoundedBar/>} dot />
+          <BarChart data={numbers} barCategoryGap='15%' >
+            <XAxis
+              axisLine={false}
+              color='#66829a'
+              dataKey='number'
+              interval='preserveStartEnd'
+              padding={{ left: 10, right: 5 }}
+              tickSize={0}
+            />
+            <YAxis
+              axisLine={false}
+              dataKey='count'
+              padding={{ top: 12, bottom: 12 }}
+              tickSize={0}
+              width={30}
+            />
+            <Tooltip
+              animationDuration={200}
+              formatter={d => `${d} times`}
+              offset={20}
+              separator=''
+            />
+            <Bar
+              dataKey='count'
+              dot
+              shape={<RoundedBar/>}
+            />
           </BarChart>
         </ResponsiveContainer>
       </section>
--- src/components/AllPrizesChart/AllPrizesChart.js
+++ src/components/AllPrizesChart/AllPrizesChart.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import { getSortedPrizes } from 'helpers/functions'
 import { formatEUR } from 'helpers/formatters'
 import './AllPrizesChart.scss'
@@ -20,12 +20,50 @@ class AllPrizesChart extends Component {
       <section className='AllPrizesChart section'>
         <h2>{title}</h2>
         <ResponsiveContainer width='100%' height={300}>
-          <AreaChart data={prizesSorted}>
-            <XAxis dataKey='year' interval='preserveStartEnd' />
-            <YAxis dataKey='yearTotal' tickFormatter={d => d / 1000000} unit=' M€' />
-            <Tooltip formatter={d => formatEUR(d)} />
-            <Area dataKey='yearTotal' dot />
-          </AreaChart>
+          <LineChart data={prizesSorted}>
+            <defs>
+              <linearGradient id='linear' x1='0%' y1='0%' x2='100%' y2='0%'>
+                <stop offset='0%'    stopColor='#1770af'/>
+                <stop offset='12.5%' stopColor='#267fb3'/>
+                <stop offset='25%'   stopColor='#2d8190'/>
+                <stop offset='37.5%' stopColor='#479493'/>
+                <stop offset='50%'   stopColor='#9ac5a0'/>
+                <stop offset='62.5%' stopColor='#f6dd9f'/>
+                <stop offset='75%'   stopColor='#ec756a'/>
+                <stop offset='87.5%' stopColor='#e94755'/>
+                <stop offset='100%'  stopColor='#f187c7'/>
+              </linearGradient>
+            </defs>
+            <XAxis
+              axisLine={false}
+              color='#66829a'
+              dataKey='year'
+              interval='preserveStartEnd'
+              padding={{ left: 25, right: 15 }}
+              tickSize={0}
+            />
+            <YAxis
+              axisLine={false}
+              dataKey='yearTotal'
+              padding={{ top: 20, bottom: 30 }}
+              tickFormatter={d => d / 1000000}
+              tickSize={0}
+              unit=' M€'
+              width={60}
+            />
+            <Tooltip
+              animationDuration={200}
+              formatter={d => formatEUR(d)}
+              offset={20}
+              separator=''
+            />
+            <Line
+              dataKey='yearTotal'
+              dot
+              stroke='url(#linear)'
+              type='monotone'
+            />
+          </LineChart>
         </ResponsiveContainer>
       </section>
     )
